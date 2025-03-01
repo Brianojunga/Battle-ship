6e2c60b6868755da900b66b10652ecda2c9fc76f
Brianojunga <brianojunga3@gmail.com>
2025-03-01 22:38:26 +0300

    Add core functionality of the game

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,11 +1,17 @@
-
+import { Ship } from "./battleship"
 export class Gameboard{
     constructor (){
         this.board = new Map()
         this.missedAttacks = new Set ()
+        this.successfulAttacks = new Set()
     }
 
-    placeShip(x,y , ship, orientation = 'horizontal'){
+    placeShip(x,y, length, orientation = 'horizontal'){
+        const ship = new Ship(length)
+        if(this.board.has(`${x},${y}`)){
+            alert('The coordinate has a Ship')
+            return 
+        }
         for(let i = 0 ; i < ship.length; i++){
             let newX = orientation === 'horizontal' ? x + i : x
             let newY = orientation === 'horizontal' ? y : y + i
@@ -17,6 +23,7 @@ export class Gameboard{
         let key = `${x},${y}`
         if(this.board.has(key)){
             this.board.get(key).hit()
+            this.successfulAttacks.add(key)
             return true
         }else{
             this.missedAttacks.add(key)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,84 @@
 import "./styles.css";
+import { Player } from "./player";
+
+const playerBoard = document.querySelector('.player-board');
+const computerBoard = document.querySelector('.computer-board');
+
+const boardSize = 10;
+let player1 = new Player ('Ojay');
+let computerPlayer = new Player ('computer');
+let currentPlayer = 'player'
+
+function setUpShips(player){
+    let shipCoords = ["1,1", "4,2", "7,8", "5,5", "3,4"];
+    for(let i = 0; i < shipCoords.length; i++){
+        let [x, y] = shipCoords[i].split(',').map(Number);
+        player.gameboard.placeShip(x,y, (i+1))
+    }
+}
+
+function createPlayerBoards(player, playerDiv){
+    for(let row = 0; row < boardSize; row++){
+        for(let col = 0; col < boardSize; col++){
+            let key = `${row},${col}`
+            const playerCell = document.createElement('div');
+            playerCell.dataset.id = key
+            playerCell.classList.add('cell');
+            if(player.gameboard.board.has(key) && player.type !== "computer") playerCell.classList.add('ship');
+            playerDiv.appendChild(playerCell)
+        }
+    }
+}
+function attack(x, y, cellClick){
+    if(currentPlayer === 'player'){
+       const computerAttacked = player1.attack(computerPlayer, x, y)
+       if(computerAttacked){
+            cellClick.classList.add('hit')
+       }else{
+            cellClick.classList.add('miss')
+       }
+    }
+
+    currentPlayer = 'computer'
+
+    setTimeout(computerAttack, 1000)
+}
+
+function computerAttack(){
+    if(currentPlayer !== 'computer') return
+
+        const playerAttacked = computerPlayer.computerAttack(player1);
+        if(playerAttacked){
+            const successAttacks = [...player1.gameboard.successfulAttacks]
+            successAttacks.forEach((key)=>{
+            const cell = document.querySelector(`.player-board .cell[data-id="${key}"]`)
+            cell.replaceChildren()
+            cell.classList.remove('ship')
+            cell.classList.add('hit')
+           }) 
+        }else{
+            const missAttacks = [...player1.gameboard.missedAttacks]
+            missAttacks.forEach(key =>{
+            const cell = document.querySelector(`.player-board .cell[data-id="${key}"]`)
+            cell.replaceChildren()
+            cell.classList.add('miss') 
+            })
+        }
+
+    currentPlayer = 'player'
+}
+
+setUpShips(player1);
+setUpShips(computerPlayer);
+createPlayerBoards(player1, playerBoard)
+createPlayerBoards(computerPlayer, computerBoard)
+
+computerBoard.addEventListener('click', e =>{
+    const cellClick = e.target.closest('[data-id]')
+    const coordinate = cellClick.dataset.id
+    const [x, y] = coordinate.split(',').map(Number)
+    attack(x, y,cellClick)
+    
+})
+
+
