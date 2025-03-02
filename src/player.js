34976cffc334e5ff323145192b941ad29d27716b
Brianojunga <brianojunga3@gmail.com>
2025-03-02 23:06:02 +0300

    Make computer generate new coordinate if the coordinate is attacked

--- src/player.js
+++ src/player.js
@@ -10,9 +10,15 @@ export class Player{
         return opponent.gameboard.receiveAttack(x,y)
     }
 
-    computerAttack(opponent){
-        let x = ~~(Math.random() * 10)
-        let y = ~~(Math.random() * 10)
-        return opponent.gameboard.receiveAttack(x,y)
+    computerAttack(opponent) {
+        let x, y, key;
+    
+        do {
+            x = Math.floor(Math.random() * 10);
+            y = Math.floor(Math.random() * 10);
+            key = `${x},${y}`;
+        } while (opponent.gameboard.missedAttacks.has(key) || opponent.gameboard.successfulAttacks.has(key));
+    
+        return opponent.gameboard.receiveAttack(x, y);
     }
 }
